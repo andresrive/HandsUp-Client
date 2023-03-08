58004a845cee3d069de3d05f59a29320394c1ad0
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-03-08 17:57:34 +0100

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,44 +1,53 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../../context/auth.context";
+import mountainLarge from '../';
 
 function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    const isScrolling = () => {
+      const headerScroll = document.querySelector('.primary-header')
+      let windowPosition = window.scrollY > 250
+      headerScroll.classList.toggle('active', windowPosition)
+    }
+    window.addEventListener('scroll', isScrolling)
+
+    return () => {
+      window.removeEventListener('scroll', isScrolling)
+    }
+  }, [])
 
   return (
-    <nav>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+    <header className="primary-header" ref={headerRef}>
+			<nav className="navbar container">
+				<Link to="#" className="logo">HandsUp 🙌🏼.</Link>
+				<ul className="nav-list">
+					<li>
+						<Link to="#" className="nav-link">Home</Link>
+					</li>
+					<li>
+						<Link to="#" className="nav-link">Pricing</Link>
+					</li>
+					<li>
+						<Link to="#" className="nav-link">Dates</Link>
+					</li>
+					<li>
+						<Link to="#" className="nav-link">Testimonials</Link>
+					</li>
+					<li>
+						<Link to="#" className="nav-link">Contact</Link>
+					</li>
+				</ul>
+				<Link to="#" className="button">Book now</Link>
+			</nav>
+		</header>
 
-      {isLoggedIn && (
-        <>
-          <button onClick={logOutUser}>Logout</button>
 
-          <Link to="/profile">
-            <button>Profile</button>
-            {/* <img src="https://picsum.photos/id/402/200/300" style={{ width: 50, height: 50, borderRadius: 25}} alt="profile" /> */}
-          </Link>
-
-          <span>{user && user.name}</span>
-        </>
-      )}
-
-      {!isLoggedIn && (
-        <>
-          <Link to="/signup">
-            {" "}
-            <button>Sign Up</button>{" "}
-          </Link>
-          <Link to="/login">
-            {" "}
-            <button>Login</button>{" "}
-          </Link>
-        </>
-      )}
-    </nav>
-  );
+  )
 }
 
 export default Navbar;
